ca566896bffefc5f8eac6adead2e3f9ca290b89e
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-06-11 04:32:53 +0600

    updated

--- src/Model/Entry.php
+++ src/Model/Entry.php
@@ -31,14 +31,7 @@ public function toArray():array
 }
 
 
-/*
-: self: This indicates that the function returns an instance of the Entry class itself (denoted by self).
 
-Purpose:
-
-This fromArray method provides a convenient way to create an Entry object from an existing associative array containing the necessary data (amount, category, date). This allows you to populate Entry objects from various sources like databases or external data feeds that provide information in an array format.
-
-*/ 
 public static function fromArray(array $data):self
 {
     return new self($data["amount"],$data["category"],$data["date"]);
